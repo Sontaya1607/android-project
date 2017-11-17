0486c1308044949938e641020a05cb1808eff28b
Sontaya58160434 <58160434@go.buu.ac.th>
2017-11-17 16:39:18 +0700

    Can OnClick All Button

--- app/src/main/java/com/example/sontaya16/karya/MainActivity.java
+++ app/src/main/java/com/example/sontaya16/karya/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sontaya16.karya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//  QUIZ BUTTON
+        Button btnQuiz = (Button)findViewById(R.id.btn_quiz);
+        btnQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), QuizActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+//  GREEN BIN BUTTON
+        Button btnGreen = (Button)findViewById(R.id.btn_green);
+        btnGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), GreenActivity.class);
+                startActivity(i);
+            }
+        });
+
+//  BLUE BIN BUTTON
+        Button btnBlue = (Button)findViewById(R.id.btn_blue);
+        btnBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), BlueActivity.class);
+                startActivity(i);
+            }
+        });
+
+//  YELLOW BIN BUTTON
+        Button btnYellow = (Button)findViewById(R.id.btn_yellow);
+        btnYellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), YellowActivity.class);
+                startActivity(i);
+            }
+        });
+
+//  RED BIN BUTTON
+        Button btnRed = (Button)findViewById(R.id.btn_red);
+        btnRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), RedActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/sontaya16/karya/YellowActivity.java
@@ -0,0 +1,13 @@
+package com.example.sontaya16.karya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YellowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yellow);
+    }
+}
